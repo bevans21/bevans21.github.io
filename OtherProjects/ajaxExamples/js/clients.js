f4ac92cc96dcb4be85ec538266445eace7730b78
bevans21 <ebronwyn21@gmail.com>
2016-03-15 11:34:27 -0400

    semicolons!

--- OtherProjects/ajaxExamples/js/clients.js
+++ OtherProjects/ajaxExamples/js/clients.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 $("#getClients").on("click", function(){
-  $.getJSON("http://bevans21.github.io/OtherProjects/ajaxExamples/jsondatabase/clients.json",function(data){
+  $.getJSON("http://bevans21.github.io/OtherProjects/ajaxExamples/jsondatabase/clients.json",function(data) {
 
     alert(data);
     console.dir(data);
-  });
+  })
 })
-})
+});
