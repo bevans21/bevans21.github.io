b25308c25fa4437e8e4bc1ecdffd94b145d7d007
bevans21 <ebronwyn21@gmail.com>
2016-03-15 11:58:57 -0400

    print table on click

--- OtherProjects/ajaxExamples/js/clients.js
+++ OtherProjects/ajaxExamples/js/clients.js
@@ -1,10 +1,14 @@
+
+
 $(document).ready(function(){
 $("#getClients").on("click", function(){
   $.getJSON("http://bevans21.github.io/OtherProjects/ajaxExamples/jsondatabase/clients.json",function(data) {
-
+    var html = "<table>" + "<tr><th>Name</th><th>Email</th><th><Company/th></tr>";
     $.each(data, function(index, item){
       $("#results").append(item.name);
     })
+    html += "</table>";
+    $("#results").append(html);
     //alert(data);
     //console.dir(data);
   })
