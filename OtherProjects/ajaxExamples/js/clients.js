8a68b7f93c02ef82889b3001cd536f4220380819
bevans21 <ebronwyn21@gmail.com>
2016-03-15 12:04:04 -0400

    table

--- OtherProjects/ajaxExamples/js/clients.js
+++ OtherProjects/ajaxExamples/js/clients.js
@@ -2,11 +2,18 @@
 
 $(document).ready(function(){
 $("#getClients").on("click", function(){
+
   $.getJSON("http://bevans21.github.io/OtherProjects/ajaxExamples/jsondatabase/clients.json",function(data) {
-    var html = "<table>" + "<tr><th>Name</th><th>Email</th><th><Company/th></tr>";
+    var html = "<table>" + "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
+
     $.each(data, function(index, item){
-      $("#results").append(item.name);
+      html += "<tr>" +
+      "<td>"+item.name"</td>" + "
+      <td>"+item.email"</td>" +
+      "<td>"+item.company"</td>" +
+      "</tr>";
     })
+
     html += "</table>";
     $("#results").append(html);
     //alert(data);
