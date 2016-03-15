a5ecc1b07a100faedaac3e7f1ee5382bfb245906
bevans21 <ebronwyn21@gmail.com>
2016-03-15 11:32:06 -0400

    fix

--- OtherProjects/ajaxExamples/js/clients.js
+++ OtherProjects/ajaxExamples/js/clients.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 $("#getClients").on("click", function(){
-  $.getJSON("http://bevans21.github.io/OtherProjects/ajaxExamples/jsondatabase/clients.json"){
+  $.getJSON("http://bevans21.github.io/OtherProjects/ajaxExamples/jsondatabase/clients.json",function(data){
 
     alert(data);
     console.dir(data);
