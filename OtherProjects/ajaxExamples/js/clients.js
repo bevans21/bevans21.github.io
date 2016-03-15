f51f92b618b2acf8df8b29301f465123296c3b07
bevans21 <ebronwyn21@gmail.com>
2016-03-15 11:39:36 -0400

    testing

--- OtherProjects/ajaxExamples/js/clients.js
+++ OtherProjects/ajaxExamples/js/clients.js
@@ -2,8 +2,11 @@ $(document).ready(function(){
 $("#getClients").on("click", function(){
   $.getJSON("http://bevans21.github.io/OtherProjects/ajaxExamples/jsondatabase/clients.json",function(data) {
 
-    alert(data);
-    console.dir(data);
+    $.each(data, function(index, item){
+      $("#results").append(item.name);
+    })
+    //alert(data);
+    //console.dir(data);
   })
 })
 });
