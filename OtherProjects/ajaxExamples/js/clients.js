67981d976645a979a0bd9a74ed72a359ef4ee9a4
bevans21 <ebronwyn21@gmail.com>
2016-03-15 11:30:48 -0400

    alert is back

--- OtherProjects/ajaxExamples/js/clients.js
+++ OtherProjects/ajaxExamples/js/clients.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 $("#getClients").on("click", function(){
-  $.getJSON("http://bevans21.github.io/OtherProjects/ajaxExamples/jsondatabase/clients.json")
+  $.getJSON("http://bevans21.github.io/OtherProjects/ajaxExamples/jsondatabase/clients.json"){
+
+    alert(data);
+    console.dir(data);
+  });
 })
 })
