3c93d98d8c392a45cf55975b03dc648500e9de73
bevans21 <ebronwyn21@gmail.com>
2016-03-15 23:44:09 -0400

    change

--- OtherProjects/ajaxExamples/js/friends.js
+++ OtherProjects/ajaxExamples/js/friends.js
@@ -1,19 +1,21 @@
+
+
 $(document).ready(function(){
-  $("#getFriends").on("click", function(){
+$("#getClients").on("click", function(){
 
-$.getJSON("http://bevans21.github.io/OtherProjects/ajaxExamples/jsondatabase/friends.json",function(data) {
-  var html = "<table class='table table-hover table-striped'>" + "<tr><th>Name</th><th>Gender</th><th>Phone Number</th></tr>";
+  $.getJSON("http://bevans21.github.io/OtherProjects/ajaxExamples/jsondatabase/clients.json",function(data) {
+    var html = "<table class='table table-hover table-striped'>" + "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
 
-  $.each(data, function(index, item){
-    html += "<tr>" +
-    "<td>" + item.name + "</td>" +
-    "<td>" + item.gender + "</td>" +
-    "<td>" + item.phone + "</td>" +
-    "</tr>";
+    $.each(data, function(index, item){
+      html += "<tr>" +
+      "<td>" + item.name + "</td>" +
+      "<td>" + item.email + "</td>" +
+      "<td>" + item.company + "</td>" +
+      "</tr>";
+    })
+
+    html += "</table>";
+    $("#results").append(html);
   })
-
-  html += "</table>";
-  $("#friends").append(html);
-})
 })
 });
