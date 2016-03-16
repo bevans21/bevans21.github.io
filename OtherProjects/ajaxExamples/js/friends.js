c5a77d705a84c1feeda000b31f97d14b9906c12b
bevans21 <ebronwyn21@gmail.com>
2016-03-16 00:39:50 -0400

    stupid ul

--- OtherProjects/ajaxExamples/js/friends.js
+++ OtherProjects/ajaxExamples/js/friends.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 $("#getFriends").on("click", function(){
 
   $.getJSON("http://bevans21.github.io/OtherProjects/ajaxExamples/jsondatabase/friends.json",function(data) {
-    var html = "<ull>Friends<li><ul><li></li><li></li>/ul></li></ul>"
+    var html = "<ul>Friends<li><ul><li></li><li></li>/ul></li></ul>"
     $.each(data, function(index, item){
       html += "<ul>" +
       "<li>" + item.name + "</li>" +
