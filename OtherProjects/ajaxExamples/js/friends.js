2b8a4be60f08147484b1f3c5f8113ec755c359f5
bevans21 <ebronwyn21@gmail.com>
2016-03-16 00:44:24 -0400

    what happens if

--- OtherProjects/ajaxExamples/js/friends.js
+++ OtherProjects/ajaxExamples/js/friends.js
@@ -4,13 +4,13 @@ $(document).ready(function(){
 $("#getFriends").on("click", function(){
 
   $.getJSON("http://bevans21.github.io/OtherProjects/ajaxExamples/jsondatabase/friends.json",function(data) {
-    var html = "<ul>Friends<li><ul><li></li><li></li>/ul></li></ul>"
+    var html = "<ul>" + "<li><ul><li></li><li></li>/ul></li></ul>"
     $.each(data, function(index, item){
       html += "<ul>" +
       "<li>" + item.name + "</li>" +
       "<li>" + item.gender + "</li>" +
       "<li>" + item.phone + "</li>" +
-       + "</ul>";
+      "</li>" + "</ul>";
     })
 
     html += "</ul>";
