e43b25582143382bb0f8d474c207faf0183e0f6a
bevans21 <ebronwyn21@gmail.com>
2016-03-15 23:36:17 -0400

    change

--- OtherProjects/ajaxExamples/js/friends.js
+++ OtherProjects/ajaxExamples/js/friends.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
   $("#getFriends").on("click", function(){
 
 $.getJSON("http://bevans21.github.io/OtherProjects/ajaxExamples/jsondatabase/friends.json",function(data) {
-  var html = "<table class='table table-hover table-striped'>" + "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
+  var html = "<table class='table table-hover table-striped'>" + "<tr><th>Name</th><th>Gender</th><th>Phone Number</th></tr>";
 
   $.each(data, function(index, item){
     html += "<tr>" +
     "<td>" + item.name + "</td>" +
-    "<td>" + item.email + "</td>" +
-    "<td>" + item.company + "</td>" +
+    "<td>" + item.gender + "</td>" +
+    "<td>" + item.phone + "</td>" +
     "</tr>";
   })
 
