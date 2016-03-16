eeb72d28111c9938638d4258607f3d0d772eb642
bevans21 <ebronwyn21@gmail.com>
2016-03-15 23:54:14 -0400

    friends

--- OtherProjects/ajaxExamples/js/friends.js
+++ OtherProjects/ajaxExamples/js/friends.js
@@ -1,7 +1,7 @@
 
 
 $(document).ready(function(){
-$("#getClients").on("click", function(){
+$("#getFriends").on("click", function(){
 
   $.getJSON("http://bevans21.github.io/OtherProjects/ajaxExamples/jsondatabase/clients.json",function(data) {
     var html = "<table class='table table-hover table-striped'>" + "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
