9145cb3689addc6cd11fff558fc2c59d52943ec8
bevans21 <ebronwyn21@gmail.com>
2016-03-16 00:37:56 -0400

    li

--- OtherProjects/ajaxExamples/js/friends.js
+++ OtherProjects/ajaxExamples/js/friends.js
@@ -6,13 +6,11 @@ $("#getFriends").on("click", function(){
   $.getJSON("http://bevans21.github.io/OtherProjects/ajaxExamples/jsondatabase/friends.json",function(data) {
     var html = "<ull>Friends<li><ul><li></li><li></li>/ul></li></ul>"
     $.each(data, function(index, item){
-      html += "<ul>" +
+      html += "<ul>" + "<li>"
       "<ul>" + item.name + "</ul>" +
       "<li>" + item.gender + "</li>" +
       "<li>" + item.phone + "</li>" +
-
-
-      "</ul>";
+      "</li>" + "</ul>";
     })
 
     html += "</ul>";
