33031326581a3db26969366d8150b0e966c34b9d
bevans21 <ebronwyn21@gmail.com>
2016-04-09 14:30:18 -0400

    change

--- OtherProjects/FinalProject/js/final.js
+++ OtherProjects/FinalProject/js/final.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
 
             $.each(data, function(index, item) {
                 html += '<div class="col-xs-12 col-md-4 jsonCats">' +
-                  '<img class="catImage" src="' + item.image + '"/>' +
+                  '<img class="catImage" src="fastbreak.png' + item.image + '"/>' +
                   //deleted commentsContainer
                   '<div class="panel panel-default">' + //added
                   '<div class="panel-heading">Renter Comments</div>'; //added
