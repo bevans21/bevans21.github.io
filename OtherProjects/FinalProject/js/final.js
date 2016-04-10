a3ea620256b294ad89b7fc4add36eaa78e033d5a
bevans21 <ebronwyn21@gmail.com>
2016-04-10 01:57:17 -0400

    hello?

--- OtherProjects/FinalProject/js/final.js
+++ OtherProjects/FinalProject/js/final.js
@@ -32,6 +32,7 @@ $(document).ready(function() {
                 html += '<div class="col-xs-12 col-md-4 jsonCats">' +
                   '<img class="catImage" src="' + item.image +'"/>' +
                   '<img class="catImage2" src="' + item.image2 +'"/>' +
+                  item.paragraph +
                   //deleted commentsContainer
                   '<div class="panel panel-default">' + //added
                   '<div class="panel-heading">Renter Comments</div>'; //added
